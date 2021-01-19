7d9908094eb5a4268b4934c5790e039e3bc317f8
Shelna-GCP <66944601+Shelna-GCP@users.noreply.github.com>
2021-01-19 17:26:52 +0530

    Update test.spec.js

--- src/__test__/test.spec.js
+++ src/__test__/test.spec.js
@@ -3,3 +3,18 @@ describe("Testing our nodeJS app", () => {
     expect(2).toBe(2);
   });
 });
+describe("Testing our nodeJS app", () => {
+  it("Testing using Github Actions", () => {
+    expect(2).toBe(4);
+  });
+});
+describe("Testing our nodeJS app", () => {
+  it("Testing using Github Actions", () => {
+    expect(23).toBe(2);
+  });
+});
+describe("Testing our nodeJS app", () => {
+  it("Testing using Github Actions", () => {
+    expect(42).toBe(42);
+  });
+});
