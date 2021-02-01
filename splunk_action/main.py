7567f76ef3e5bea18c7d0a1f6b1d9579f989f93d
Shelna-GCP <66944601+Shelna-GCP@users.noreply.github.com>
2021-02-01 21:17:44 +0530

    Update main.py

--- splunk_action/main.py
+++ splunk_action/main.py
@@ -26,7 +26,7 @@ def get_timestamp(date_time, template='%Y-%m-%dT%H:%M:%SZ'):
 def collect_build_data():
     print("Collecting Build Data ... ")
     
-    print(f"{GITHUB_API_URL}/repos/{GITHUB_REPOSITORY}/actions/runs/{GITHUB_RUN_ID})
+    print(f"{GITHUB_API_URL}/repos/{GITHUB_REPOSITORY}/actions/runs/{GITHUB_RUN_ID}")
     rundata = requests.get(f"{GITHUB_API_URL}/repos/{GITHUB_REPOSITORY}/actions/runs/{GITHUB_RUN_ID}", headers=header)
     print (rundata.json())
     print(f"{GITHUB_API_URL}/repos/{GITHUB_REPOSITORY}/actions/runs/{GITHUB_RUN_ID}")
