0737aa2c7a9fa80d0083f539b1844d920c518809
Shelna-GCP <66944601+Shelna-GCP@users.noreply.github.com>
2021-02-01 18:38:24 +0530

    Update main.py

--- splunk_action/main.py
+++ splunk_action/main.py
@@ -25,6 +25,10 @@ def get_timestamp(date_time, template='%Y-%m-%dT%H:%M:%SZ'):
 
 def collect_build_data():
     print("Collecting Build Data ... ")
+    
+    artifactresponse = requests.get(f"{GITHUB_API_URL}/repos/{GITHUB_REPOSITORY}/actions/artifacts, headers=header)
+    print (artifactresponse.json())
+                                 
     print("JUNIT result : ", JUNIT_REPORT)
     with open(JUNIT_REPORT , 'r+') as jreport:
         print(jreport.read())
