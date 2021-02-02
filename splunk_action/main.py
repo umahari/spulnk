1848b8e199971fa34128a20db2213d5cb4073a50
Shelna-GCP <66944601+Shelna-GCP@users.noreply.github.com>
2021-02-02 13:32:23 +0530

    Update main.py

--- splunk_action/main.py
+++ splunk_action/main.py
@@ -43,7 +43,7 @@ def collect_build_data():
         downloadartifact = requests.get(f"{GITHUB_API_URL}/repos/{GITHUB_REPOSITORY}/actions/artifacts/{id}/zip", headers=header)
         requests.delete(f"{GITHUB_API_URL}/repos/{GITHUB_REPOSITORY}/actions/artifacts/{id}", headers=header)
     
-    with zipfile.ZipFile('Unit Test Results Splunk.zip', 'r') as zip_ref:
+    with zipfile.ZipFile('artifacts/Unit Test Results Splunk.zip', 'r') as zip_ref:
         zip_ref.extractall('Unit Test Results Splunk')
     
     
